69ca5021c0496d23526b7999de2bab1df37a187f
Amine Graja <93081308+Amingraja@users.noreply.github.com>
2025-10-01 00:52:52 +0100

    Add Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="bowel-sound-classification",
+    version="0.1.0",
+    author="Amin Graja",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ]
+)

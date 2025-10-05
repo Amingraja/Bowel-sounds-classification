e99191a9f546386be36b8d26345d94e9435fcbec
Amine Graja <93081308+Amingraja@users.noreply.github.com>
2025-10-06 00:25:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,17 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-    ]
+        "datasets==4.1.1",
+        "matplotlib==3.7.1",
+        "numpy==1.25.1",
+        "pandas==2.3.3",
+        "pydub==0.25.1",
+        "PyYAML==6.0.1",
+        "scikit_learn==1.7.2",
+        "seaborn==0.13.2",
+        "torch==2.8.0",
+        "torchaudio==2.8.0",
+        "transformers==4.27.3"
+    ],
 )
+

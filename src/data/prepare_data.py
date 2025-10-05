54bc5e64635ba370ff1e2d7a52d243490ff6c197
Amine Graja <93081308+Amingraja@users.noreply.github.com>
2025-10-06 00:24:17 +0100

    Update prepare_data.py

--- src/data/prepare_data.py
+++ src/data/prepare_data.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
 
     train_df, val_df = data_balancer.balance_train_val()
     test_df = data_balancer.balance_test()
-    print(f"📊 Train samples: {len(train_df)}, Validation samples: {len(val_df)}, Test samples: {len(test_df)}")
+    print(f"Train samples: {len(train_df)}, Validation samples: {len(val_df)}, Test samples: {len(test_df)}")
     print("Class distribution (train):\n", train_df["label"].value_counts())
     train_df.to_csv(config["train"], index=False)
     test_df.to_csv(config["test"], index=False)
     val_df.to_csv(config["val"], index=False)
+
